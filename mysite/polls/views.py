2da8e249459bc7a5af4355df74937323a63bc474
Michael Boesl <michael.boesl@t-online.de>
2020-04-13 13:54:49 +0200

    finish voting logic

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 import json
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -24,9 +25,29 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # We are using the reverse() function in the HttpResponseRedirect constructor in this example.
+        # This function helps avoid having to hardcode a URL in the view function.It is given the name of the
+        # view that we want to pass control to and the variable portion of the URL pattern that points to that view.
+        # In this case, using the URLconf we set up in Tutorial 3, this reverse() call will return a string like
+        # /polls/3/results/
