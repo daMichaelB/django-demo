9a93d1e78878dcf29ec2cf7b3c07e6cfc94f3d92
Michael Boesl <michael.boesl@t-online.de>
2020-04-19 20:53:40 +0200

    show posts by tag

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,8 +6,11 @@ app_name = 'blog'
 # TODO: Check: does Django only define the endpoints here and we have to switch POST/GET/PUT/DELETE inside the view??
 urlpatterns = [
     # post views
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    # You use a slug path converter to match the parameter as a lowercase string with ASCII letters or numbers,
+    # plus the hyphen and underscore characters.
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    # path('', views.PostListView.as_view(), name='post_list'),
     # <slug:post> to specifically match a slug.
     # see also: https://docs.djangoproject.com/en/3.0/topics/http/urls/#path-converters.
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,35 +1,42 @@
 from django.core.mail import send_mail
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from .forms import EmailPostForm, CommentForm
-from .models import Post, Comment
+from .models import Post
 
 
-class PostListView(ListView):
-    """
-    same as the .post_list() function
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+#class PostListView(ListView):
+#    """
+#    same as the .post_list() function
+#    """
+#    queryset = Post.published.all()
+#    context_object_name = 'posts'
+#    paginate_by = 3
+#    template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#    object_list = Post.published.all()
-#    paginator = Paginator(object_list, 3)  # 3 posts in each page
-#    page = request.GET.get('page')
-#    try:
-#        posts = paginator.page(page)
-#    except PageNotAnInteger:
-#        # If page is not an integer deliver the first page
-#        posts = paginator.page(1)
-#    except EmptyPage:
-#        # If page is out of range deliver last page of results
-#        posts = paginator.page(paginator.num_pages)
-#    print(page)
-#    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    print(page)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
